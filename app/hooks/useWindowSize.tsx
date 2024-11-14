8b5863e03f315f2febab653cddde22fe5f9d48dd
yonela-johannes <johannesyonela@gmail.com>
2024-11-14 23:24:23 +0200

    chore: window size

--- app/hooks/useWindowSize.tsx
+++ app/hooks/useWindowSize.tsx
@@ -2,15 +2,11 @@ import {useEffect, useState} from 'react';
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
     isMobile: false,
   });
 
   const handleSize = () => {
     setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
       isMobile: window.innerWidth < 1024,
     });
   };
